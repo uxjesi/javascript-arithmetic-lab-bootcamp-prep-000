625e6666875b0c96d84175645af78516fdd88f14
Jessica Maria Loredo <jesi.thegreat@gmail.com>
2018-07-07 22:49:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 var a = 5
 var b = 10
+var n = 2
 
 function add(a, b) {
   return a += b
 }
 
+function subtract (a, b) {
+  return a-= b
+}
+
+function multiply(a, b) {
+  return a *= b
+}
+
+function divide(a, b) {
+  return a /= b 
+}
+
+function inc(n) {
+  n
+}
